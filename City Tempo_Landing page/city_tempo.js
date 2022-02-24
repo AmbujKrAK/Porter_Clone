b9ebdb3013d3fcf76fb13559a0848be68d947757
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-02-24 18:22:24 +0530

    Bug fix

--- City Tempo_Landing page/city_tempo.js
+++ City Tempo_Landing page/city_tempo.js
@@ -1,5 +1,5 @@
 
 import navbar from "./components/header.js";
-
+console.log(navbar);
 let nav = document.getElementById('main');
 nav.innerHTML = navbar();
--- City Tempo_Landing page/components/header.js
+++ City Tempo_Landing page/components/header.js
@@ -1,40 +1,42 @@
-function navbar(){
+// function navbar(){
 
-return ` <div id="header">
-<div class="logoMain">
-  <img src="https://i.ibb.co/R3W8SSW/Untitled1.png" alt="">
-</div>
-<div class="tagline">
-  <p>India's Largest Marketplace for Intracity Logistics</p>
-</div>
-<div class="join">
-  <img src="https://d35ppnqksufxmo.cloudfront.net/frontend_website/9e4ccae9c5a1b669d8fa2648db220c3f.svg"
-    alt="Please Refresh Page">
-    <span>Drive With US</span>
-    <span>turn kms into money</span>
-  <span>JOIN US</span>
-</div>
-<div class="logoCall">
-  <img src="https://cdn-icons-png.flaticon.com/128/126/126509.png" alt="">
-</div>
-<div id="contact">
-  <h4>4410-4410</h4><span>add your city code</span>
-</div>
-<hr>
-</div>
-<div id="navbar">
-<div class="left">
-  <div><a href="">CITY TEMPO</a></div>
-  <div><a href="">PACKERS & MOVERS</a></div>
-  <div><a href="">DRIVER PARTNERS</a></div>
-  <div><a href="">FOR ENTERPRISE</a></div>
-</div>
-<div class="right">
-  <div>Support</div>
-  <div>Track Order</div>
-</div>
-</div>
-<hr> `
-}
+// return `<div id="header">
+//       <div class="logoMain">
+//       <img src="https://i.ibb.co/R3W8SSW/Untitled1.png" alt="">
+//     </div>
+//     <div class="tagline">
+//       <p>India's Largest Marketplace for Intracity Logistics</p>
+//     </div>
+//     <div class="join">
+//       <img src="https://d35ppnqksufxmo.cloudfront.net/frontend_website/9e4ccae9c5a1b669d8fa2648db220c3f.svg"
+//         alt="Please Refresh Page">
+//         <div class="tag_logo">
+//       <b>Drive With US</b>
+//       <span class="sml"><b>turn kms into money</b></span>
+//     </div>
+//     <b class="joinUS">JOIN US</b>
+//     </div>
+//     <div class="logoCall">
+//       <img src="https://cdn-icons-png.flaticon.com/128/126/126509.png" alt="">
+//     </div>
+//     <div id="contact">
+//       <h4>4410-4410</h4><span>add your city code</span>
+//     </div>
+//     <hr>
+//   </div>
+//   <div id="navbar">
+//     <div class="left">
+//       <div><a href="">CITY TEMPO</a></div>
+//       <div><a href="">PACKERS & MOVERS</a></div>
+//       <div><a href="">DRIVER PARTNERS</a></div>
+//       <div><a href="">FOR ENTERPRISE</a></div>
+//     </div>
+//     <div class="right">
+//       <div>Support</div>
+//       <div>Track Order</div>
+//     </div>
+//   </div>
+//   <hr> `
+// }
 
-export default navbar;
+// export default navbar;
